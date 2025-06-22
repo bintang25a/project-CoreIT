19abee75257d19076e2aa2b87838ce4ccb34cd90
bintang25a <bintangalfizar25@gmail.com>
2025-06-23 00:25:07 +0700

    FINAL 100% FIX

--- src/_api/index.js
+++ src/_api/index.js
@@ -14,6 +14,7 @@ API.interceptors.request.use(
       if (token) {
          config.headers["Authorization"] = `Bearer ${token}`;
       }
+      config.headers["ngrok-skip-browser-warning"] = "true";
       return config;
    },
    (error) => Promise.reject(error)
