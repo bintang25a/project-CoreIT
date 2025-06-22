5c30c92a9be94e5327565bbed87ddcde3ab14a4b
bintang25a <bintangalfizar25@gmail.com>
2025-06-22 18:06:11 +0700

    FINAL 100% FIX

--- src/layouts/admin.jsx
+++ src/layouts/admin.jsx
@@ -4,8 +4,8 @@ import Footer from "../components/admin/Footer";
 import MobileProtected from "../components/admin/MobileProtected";
 import useLoadingSpinner from "../components/elements/LoadingModal.jsx";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { isAuthenticated } from "../_services/auth";
+import { useState, useEffect, useRef } from "react";
+import { isAuthenticated, logout } from "../_services/auth";
 import { getDivisions, getDivisionLogo } from "../_services/divisions";
 import { getImages, getImageUrl } from "../_services/galleries";
 import { getMembers } from "../_services/members";
@@ -16,6 +16,7 @@ import "./admin.css";
 export default function AdminLayout() {
    const navigate = useNavigate();
    const location = useLocation();
+   const timeoutRef = useRef();
    const currentPath = location.pathname;
    const [divisions, setDivisions] = useState([]);
    const [divisionsLogo, setDivisionsLogo] = useState("");
@@ -29,6 +30,7 @@ export default function AdminLayout() {
 
    const { loading, LoadingSpinner } = useLoadingSpinner();
 
+   //Mengambil data
    const fetchData = async () => {
       if (currentPath.startsWith("/admin/members")) {
          const [membersData, divisionsData, divisionsLogoData] =
@@ -107,6 +109,7 @@ export default function AdminLayout() {
       }
    };
 
+   // Cek validasi token
    useEffect(() => {
       const checkAuth = async () => {
          const valid = await isAuthenticated();
@@ -120,6 +123,28 @@ export default function AdminLayout() {
       };
       checkAuth();
    }, [navigate]);
+
+   // Auto logout setelah 10 menit
+   const resetTimer = () => {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = setTimeout(() => {
+         logout();
+         navigate("/login", { replace: true });
+      }, 5 * 60 * 1000);
+   };
+   useEffect(() => {
+      const events = ["mousemove", "keydown", "click", "scroll"];
+      events.forEach((event) => window.addEventListener(event, resetTimer));
+
+      resetTimer();
+
+      return () => {
+         events.forEach((event) =>
+            window.removeEventListener(event, resetTimer)
+         );
+         clearTimeout(timeoutRef.current);
+      };
+   }, []);
 
    return (
       <>
